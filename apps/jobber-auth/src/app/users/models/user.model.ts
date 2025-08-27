0fba975ecbaa896a4ba31411a0b0c49876772919
hitesh-s0lanki <hiteshsolanki4623@gmail.com>
2025-08-27 19:16:33 +0530

    -- updated

--- apps/jobber-auth/src/app/users/models/user.model.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractModel } from '@jobber-microservice-app/nestjs';
-import { Field, ObjectType } from '@nestjs/graphql';
-
-@ObjectType()
-export class User extends AbstractModel {
-  @Field()
-  email: string;
-}
